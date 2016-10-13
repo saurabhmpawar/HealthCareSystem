008df128722b74b589799b919e1a6bd031723e34
pavan_pawar <pavanpawar4591@gmail.com>
2016-10-13 19:50:18 +0530

    added library

---
+++ java/HealthCareSystem/src/com/healtcare/SmsSending.java
@@ -0,0 +1,40 @@
+package com.healtcare;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+
+public class SmsSending extends JInternalFrame implements ActionListener {
+
+
+
+  private JLabel lblSenerPhone;
+  private JLabel lblTxt;
+  
+  private JTextArea txtPhonNo;
+  private JTextArea txtText;
+  
+  public SmsSending(JFrame getParentFrame)
+  {
+    
+    
+    
+  }
+  
+  
+  
+
+
+
+  @Override
+  public void actionPerformed(ActionEvent event) {
+
+
+
+  }
+
+}
